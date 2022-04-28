8b1eb4842937bf352cdbf648a9ec40060a54ceb7
Bruno Jaques Olivera de Vargas <70863493+bjvargas@users.noreply.github.com>
2022-04-28 10:28:08 -0300

    [Feature  1] - Parsing the movies
    Parsing the JsonString to String[] movies and get their attributes. (Using just JRE without external libraries)

--- src/main/java/CallApi.java
+++ src/main/java/CallApi.java
@@ -7,10 +7,11 @@ import java.net.http.HttpHeaders;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CallApi {
-
-
     private static final HttpClient httpClient = HttpClient.newBuilder()
             .version(HttpClient.Version.HTTP_1_1)
             .connectTimeout(Duration.ofSeconds(10))
@@ -18,26 +19,59 @@ public class CallApi {
 
     public static void main (String[] args) throws IOException, InterruptedException {
 
+        String[] moviesArray = parseJsonMovies(getJsonAsStringForIMDB());
+        String[] attributes = parseAttributes(moviesArray);
 
+        List<String> titles = getAttributeList(attributes, "title");
+        titles.forEach(System.out::println);
+
+        List<String> urlImages = getAttributeList(attributes, "image");
+        urlImages.forEach(System.out::println);
+
+        List<String> years = getAttributeList(attributes, "year");
+        years.forEach(System.out::println);
+
+        List<String> imDbRatings = getAttributeList(attributes, "imDbRating\"");
+        imDbRatings.forEach(System.out::println);
+
+    }
+
+    public static String getJsonAsStringForIMDB() throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://imdb-api.com/en/API/Top250Movies/{$MY_KEY}"))
+                .uri(URI.create("https://imdb-api.com/en/API/Top250Movies/" + System.getenv("KEY")))
                 .GET()
                 .build();
 
         HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
-        // print response headers
         HttpHeaders headers = response.headers();
         headers.map().forEach((k, v) -> System.out.println(k + ":" + v));
 
-        // print status code
         System.out.println(response.statusCode());
 
-        // print response body
-        System.out.println(response.body());
-
-
+       return response.body();
     }
 
+    public static String[] parseJsonMovies(String json) {
+        json = json.substring(json.indexOf("[") + 1);
+        json = json.substring(0, json.indexOf("]"));
+
+        return  json.split("},");
+    }
+
+    public static String[] parseAttributes(String[] attributes) {
+        String att = Arrays.toString(attributes);
+        return  att.split(",");
+    }
+
+    public static List<String> getAttributeList(String[] attributes, String search){
+       List<String> attList = new ArrayList<>();
+        for (String att: attributes) {
+            if(att.contains(search)){
+                attList.add(att);
+            }
+        }
+        return attList;
+    }
 
 }
